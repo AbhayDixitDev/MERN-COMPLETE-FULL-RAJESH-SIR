d5c2f6b99209d399c8480c08596990634f31fac2
Abhay Dixit <dixitshivam534@gmail.com>
2024-11-09 14:10:47 +0530

    file system use

--- NODE Intro/index.js
+++ NODE Intro/index.js
@@ -1,12 +1,34 @@
 const http = require("http");
+const fs= require("fs")
 
 // http.createServer((req, res) => {
 //     res.write("<h1>We are MERN Developer</h1>");
 //     res.end();
 // }).listen(8000);
 
-http.createServer((req,res)=>{
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("<h1> We are <br>MERN Developer</h1>");
-    res.end("<h1>this is server end<h1>");
-}).listen(8000);
+// http.createServer((req,res)=>{
+//     // res.writeHead(200,{'Content-Type':'text/html'});
+//     res.write("<h1> We are <br>MERN Developer</h1>");
+//     res.end("<h1>this is server end<h1>");
+// }).listen(8000);
+
+
+// http.createServer((req,res)=>{
+//     fs.readFile("./music.mp3",(err,result)=>{
+//         res.write(result)
+//         res.end()
+//     })
+// }).listen(8000)
+http.createServer((req, res) => {
+  if (req.url === '/video') {
+    
+    res.writeHead(200, {
+      'Content-Type': 'video/mp4'
+    });
+    
+    // Stream video file directly to response
+    fs.createReadStream("./video.mp4").pipe(res);
+  }
+}).listen(3000, () => {
+  console.log(`Server listening on port ${port}`);
+});
